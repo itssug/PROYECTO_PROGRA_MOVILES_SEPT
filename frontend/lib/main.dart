ac3d17d9032deba7a6ad18bb858f27db1071958d
itssug <parklove651@gmail.com>
2026-06-05 11:43:16 -0400

    modulo kat

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -121,8 +121,34 @@ class _HomeState extends State<Home> {
 }*/
 
 
+// ============================================================
+// ARCHIVO: lib/main.dart KATERIN
+// ============================================================
 
+// import 'package:flutter/material.dart';
+// import 'screens/actividad_fisica/actividad_fisica_screen.dart';
 
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Glucosa App - Diabetes Tipo II',
+//       theme: ThemeData(
+//         brightness: Brightness.dark,
+//         primaryColor: const Color(0xFFFF6B00),
+//         scaffoldBackgroundColor: const Color(0xFF121212),
+//       ),
+//       home: const ActividadFisicaScreen(),
+//     );
+//   }
+// }
 
 
 
